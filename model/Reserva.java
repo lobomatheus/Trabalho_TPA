29d7174ed5221df134aa2817219346115dfe92e2
Matheus Cardoso <matheus.antonio.c@outlook.com>
2019-05-22 21:06:36 -0300

    Classe modelo de Reserva adicionada

---
+++ model/Reserva.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.Calendar;
+
+public class Reserva {
+
+    private Calendar dataEntrada;
+    private int qtdDias;
+    private boolean incluiCafe;
+
+    public Reserva(){
+
+    }
+
+}
